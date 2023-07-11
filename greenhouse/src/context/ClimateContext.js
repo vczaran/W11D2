8b296094453b33570da1a4c41e493d9c9ce2186b
Viktoria Czaran <vczaran@gmail.com>
2023-07-11 13:55:28 -0400

    Completed phase 5 - hygrometer

--- greenhouse/src/context/ClimateContext.js
+++ greenhouse/src/context/ClimateContext.js
@@ -9,12 +9,16 @@ export const useClimate = () => useContext(ClimateContext);
 
 export default function ClimateProvider({ children }) {
   const [temperature, setTemperature] = useState(50);
+  const [humidity, setHumidity] = useState(40);
 
   return (
     <ClimateContext.Provider
       value={{
         temperature,
         setTemperature
+      }, {
+        humidity,
+        setHumidity
       }}
     >
       {children}
