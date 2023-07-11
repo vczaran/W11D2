0ce22864b47a9721ecb7b56ebeed434cd86ba1d0
Viktoria Czaran <vczaran@gmail.com>
2023-07-11 13:40:25 -0400

    Compelted ClimateProvider and ClimateContext

--- greenhouse/src/context/ClimateContext.js
+++ greenhouse/src/context/ClimateContext.js
@@ -1,2 +1,23 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useContext, useState } from 'react';
+
+export const ClimateContext = createContext();
+
+export const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider({ children }) {
+  const [temperature, setTemperature] = useState(50);
+
+  return (
+    <ClimateContext.Provider
+      value={{
+        temperature,
+        setTemperature
+      }}
+    >
+      {children}
+    </ClimateContext.Provider>
+  );
+}
