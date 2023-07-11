949248d254b37d60ad9742191c93049ccfa7304a
Viktoria Czaran <vczaran@gmail.com>
2023-07-11 14:38:12 -0400

    Completed thermometer timer (bonus 1)

--- greenhouse/src/components/Thermometer/Thermometer.js
+++ greenhouse/src/components/Thermometer/Thermometer.js
@@ -1,15 +1,29 @@
 import ReactSlider from "react-slider";
 import './Thermometer.css';
 import { useClimate } from "../../context/ClimateContext";
+import { useState, useEffect } from "react";
 
 function Thermometer() {
   const {temp} = useClimate();
   const [temperature, setTemperature] = temp;
+  const [actualTemp, setActualTemp] = useState(temperature);
+
+  useEffect( () => {
+    const timer = setTimeout( () => {
+      if (actualTemp < temperature) {
+        setActualTemp(actualTemp + 1)
+      } else if (actualTemp > temperature) {
+        setActualTemp(actualTemp - 1)
+      } else {
+        return () => clearTimeout(timer);
+      }
+    }, 1000);
+  }, [{temperature}]);
 
   return (
     <section>
       <h2>Thermometer</h2>
-      <div className="actual-temp">Actual Temperature: {temperature}°F</div>
+      <div className="actual-temp">Actual Temperature: {actualTemp}°F</div>
       <ReactSlider
         value={temperature}
         onAfterChange={(val) => {setTemperature(val)}}
