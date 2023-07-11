cf18c34ec41fe8f3f7b092c1ec27afac00c47e80
Viktoria Czaran <vczaran@gmail.com>
2023-07-11 16:02:52 -0400

    Working on HomePage css

--- advanced-forms/advanced-forms/src/HomePage/HomePage.js
+++ advanced-forms/advanced-forms/src/HomePage/HomePage.js
@@ -1,7 +1,9 @@
+import './HomePage.css'
+
 function HomePage () {
     return (
         <div>
-            <img id="logo" src="../_assets/logo.png" alt="logo"></img>
+            <img id="logo" src="logo.png" alt="logo" width="100" height="100"></img>
             <h1>Survey Tool</h1>
             <h4>Home</h4>
             <button>Sample Survey</button>
@@ -9,7 +11,7 @@ function HomePage () {
             <button>Report</button>
             <button id="home-button">Home</button>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <footer id="copyright">(c) Copyright ..........................................</footer>
+            <footer id="copyright">(c) Copyright .............................................................</footer>
         </div>
     )
 }
