e3e6f01779faf8deb17631c41589fc78669f8dbb
Viktoria Czaran <vczaran@gmail.com>
2023-07-11 12:02:14 -0400

    Completed phase 1

--- greenhouse/src/components/Greenhouse/Greenhouse.js
+++ greenhouse/src/components/Greenhouse/Greenhouse.js
@@ -1,22 +1,36 @@
 import dayImage from './images/greenhouse-day.jpg';
 import nightImage from './images/greenhouse-night.jpg';
 import './Greenhouse.css';
+import { useTheme } from '../../context/ThemeContext';
 
 import LightSwitch from './LightSwitch';
 import ClimateStats from './ClimateStats';
 
 function Greenhouse() {
-
+  const theme = useTheme();
+  
+  if (theme.themeName === "day") {
   return (
     <section>
-      <img  className='greenhouse-img'
+      <img  className='greenhouse-day-img'
             src={dayImage}
             alt='greenhouse' 
       />
       <LightSwitch />
       <ClimateStats />
     </section>
-  );
+  )} else {
+    return (
+      <section>
+      <img  className='greenhouse-night-img'
+            src={nightImage}
+            alt='greenhouse' 
+      />
+      <LightSwitch />
+      <ClimateStats />
+    </section>
+    )
+  }
 }
 
 export default Greenhouse;
