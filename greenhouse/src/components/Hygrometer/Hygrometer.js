b11588ab064f8d23b714247b42b0a1a74c00305c
Viktoria Czaran <vczaran@gmail.com>
2023-07-11 14:45:31 -0400

    Completed hygrometer timer - bonus 2

--- greenhouse/src/components/Hygrometer/Hygrometer.js
+++ greenhouse/src/components/Hygrometer/Hygrometer.js
@@ -1,15 +1,29 @@
 import ReactSlider from "react-slider";
 import "./Hygrometer.css";
 import { useClimate } from "../../context/ClimateContext";
+import { useState, useEffect } from 'react';
 
 function Hygrometer() {
   const {hum} = useClimate();
   const [humidity, setHumidity] = hum;
+  const [actualHum, setActualHum] = useState(humidity);
+
+  useEffect( () => {
+    const timer = setTimeout( () => {
+      if (actualHum < humidity) {
+        setActualHum(actualHum + 2)
+      } else if (actualHum > humidity) {
+        setActualHum(actualHum - 2)
+      } else {
+        return () => clearTimeout(timer);
+      }
+    }, 1000);
+  }, [{humidity}]);
 
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {humidity}%</div>
+      <div className="actual-humid">Actual Humidity: {actualHum}%</div>
       <ReactSlider
         value={humidity}
         onAfterChange={(val) => {setHumidity(val)}}
